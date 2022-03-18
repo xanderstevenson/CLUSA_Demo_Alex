520b814818e7957e9c467028c0af6e4e606c3e59
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-03-18 11:07:48 -0400

    cleaned up code indents

--- fe/src/App.js
+++ fe/src/App.js
@@ -17,9 +17,7 @@ var url = 'https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/ba
 // Index to the current question
 let index = 0
 
-//
-var userID = ''
-
+// Initialize data object
 const initialData = Object.freeze({
 	email: "",
 	first: "",
@@ -45,6 +43,7 @@ const App = () => {
 			})
 		}
 	}, []);
+
 // dislaying the questions
 	const imageHandler = (e) => {
 		if (index < questionList.length) {
@@ -60,11 +59,6 @@ const App = () => {
 	}
 
 	///// handle form input
-
-
-
-
-
 
 	  const handleChange = (event) => {
 		setData({
@@ -95,8 +89,8 @@ const App = () => {
 				first: data.first,
 				last: data.last
 			}
-			var it = objectToQueryString(reqParams)
-			axios.post(dbURL + it)
+			var uriParams = objectToQueryString(reqParams)
+			axios.post(dbURL + uriParams)
 			// api call response
 			.then((response) => {
 			console.log(response.status);
@@ -109,7 +103,6 @@ const App = () => {
 				id: response.data._id
 			})
 		})
-
 		.catch((error) => {
 			if (error.response) {
 			  console.log(error.response);
@@ -121,150 +114,149 @@ const App = () => {
 		}
 		})
 		
-	}
+}
+// end of App()
 
 
-
-	// Set the slide heading
-	var headingWords = 'Cisco DevNet Dash!'
-	// Set the button text
-	var buttonText = 'Begin!'
-    // produce header element with variable from above
-    var headingElement = <p className="heading">{headingWords}</p>
-	// produce button text with variable from above
-	var buttonElement = <button className="mainButton" onClick={imageHandler}>{buttonText}</button>
-    // hardcoded user data for testing
-	var carNumber = 3
+// Set the slide heading
+var headingWords = 'Cisco DevNet Dash!'
+// Set the button text
+var buttonText = 'Begin!'
+// produce header element with variable from above
+var headingElement = <p className="heading">{headingWords}</p>
+// produce button text with variable from above
+var buttonElement = <button className="mainButton" onClick={imageHandler}>{buttonText}</button>
+// hardcoded user data for testing
+var carNumber = 3
 
 // 5 functions to display five different pages
 	
 // page 1
-	function LandingPage() {
-		return (
-		  	<div>
-				{/* <img src='https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/backend/media/Devvie-checkered-flag.jpeg?raw=true'></img> */}
-				<button className="mainButton"><Link to="/register">Let's Register</Link></button>
-				<center><img id="devnetIcon" src="https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/purple-devnet-sharp.jpeg?raw=true" alt="Devnet log, purple"></img></center>
-			</div>
-		);
-	  }
+function LandingPage() {
+	return (
+		<div>
+			{/* <img src='https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/backend/media/Devvie-checkered-flag.jpeg?raw=true'></img> */}
+			<button className="mainButton"><Link to="/register">Let's Register</Link></button>
+			<center><img id="devnetIcon" src="https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/purple-devnet-sharp.jpeg?raw=true" alt="Devnet log, purple"></img></center>
+		</div>
+	);
+	}
 // page 2
-	  const RegisterPage = () => {
-		  headingWords = "Race Registration"
-		  setImage('https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/cars-palmtrees.jpg?raw=true')
-		   
-		  return (
+const RegisterPage = () => {
+	headingWords = "Race Registration"
+	setImage('https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/cars-palmtrees.jpg?raw=true')
+	
+	return (
 
-	// This is the form to collect user data
-			<center>
-			
-			<form name='form'>
-			<label htmlFor="fname">First Name: </label>
-				<input 
-					type="text" 
-					name="first" 
-					placeholder="Enter first name"
-					// required
-					value={data.first}
-					onChange={handleChange}
-					id="fname"
-					maxLength='25'
-					minLength='1'
-					>
-				  </input>
-			  <br></br>
-			  <label>Last Name: </label>
-			  	<input 
-				  type="text" 
-				  name="last" 
-				  value={data.last}
-				  placeholder="Enter last name" 
-				//   required
-				  onChange={handleChange}
-				  id="lname" 
-				  >
-				</input>
-			  <br></br>
-			  <label>Email Addr: </label>
-			  	<input 
-				  type="email" 
-				  name="email" 
-				  value={data.email}
-				  placeholder="Enter email" 
-				//   required
-				  onChange={handleChange}
-				  id="emailId"  
-				//   onKeyUp="emailValidate()" 
-                  >
-				</input>
-			  <br></br>
-			  <button type="submit" id="submitButton" className="mainButton" onClick={handleSubmit}><Link to="/holding-page">Register</Link></button>
-			</form> 
-  
-			</center>
-		);
-	  }
+// This is the form to collect user data
+	<center>
+	
+	<form name='form'>
+	<label htmlFor="fname">First Name: </label>
+		<input 
+			type="text" 
+			name="first" 
+			placeholder="Enter first name"
+			// required
+			value={data.first}
+			onChange={handleChange}
+			id="fname"
+			maxLength='25'
+			minLength='1'
+			>
+			</input>
+		<br></br>
+		<label>Last Name: </label>
+		<input 
+			type="text" 
+			name="last" 
+			value={data.last}
+			placeholder="Enter last name" 
+		//   required
+			onChange={handleChange}
+			id="lname" 
+			>
+		</input>
+		<br></br>
+		<label>Email Addr: </label>
+		<input 
+			type="email" 
+			name="email" 
+			value={data.email}
+			placeholder="Enter email" 
+		//   required
+			onChange={handleChange}
+			id="emailId"  
+		//   onKeyUp="emailValidate()" 
+			>
+		</input>
+		<br></br>
+		<button type="submit" id="submitButton" className="mainButton" onClick={handleSubmit}><Link to="/holding-page">Register</Link></button>
+	</form> 
+
+	</center>
+);
+}
 // page 3
-	  const HoldingPage = () => {
-		headingWords = "Your Are Car #" + carNumber
-		setImage("https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/lambo_speedometer.gif?raw=true")
-		return (
-		<center>
-		  <div>
-			<p>Welcome, {data.first}</p>
-			<p>ID # {data.id} </p> 
-			<h3>START YOUR ENGINES!</h3>
-			<button className="mainButton"><Link to="/start-page">Start!</Link></button>
-		  </div>
-		  </center>
-		);
-	  }
+const HoldingPage = () => {
+headingWords = "Your Are Car #" + carNumber
+setImage("https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/lambo_speedometer.gif?raw=true")
+return (
+<center>
+	<div>
+	<p>Welcome, {data.first}</p>
+	<p>ID # {data.id} </p> 
+	<h3>START YOUR ENGINES!</h3>
+	<button className="mainButton"><Link to="/start-page">Start!</Link></button>
+	</div>
+	</center>
+);
+}
 
 // page 4
-	function StartPage() {
-		setImage("https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/starting-light.gif?raw=true")
-		return (
-			
-		  <div>
-			<center>
-			<button  onClick={imageHandler} className="mainButton"><Link to="/race">Race!</Link></button>
-			</center>
-		  </div>
-	  );
-	  }
+function StartPage() {
+	setImage("https://github.com/xanderstevenson/CLUSA_Demo_Alex/blob/alex_local/fe/public/starting-light.gif?raw=true")
+	return (
+		
+		<div>
+		<center>
+		<button  onClick={imageHandler} className="mainButton"><Link to="/race">Race!</Link></button>
+		</center>
+		</div>
+	);
+}
 // page 5	
-	  function QuestionPage() {
-		return (
-		  <div>
-			<center>
-			  {buttonElement}
-			</center>
-		  </div>
-	  );
-	  }
+function QuestionPage() {
+return (
+	<div>
+	<center>
+		{buttonElement}
+	</center>
+	</div>
+);
+}
 
 
 // main biolerplate HTML for all pages
 
-	return (
-
-		<div className="page">
-			<div className="container">
-				{headingElement}
-				<div className="img-holder">
-					<img src={question} alt="" id="img" className="img" />
-				</div>
-				<Router>
-					<Routes>
-						<Route exact path="/" element={<LandingPage />} />
-						<Route exact path="/register" element={<RegisterPage />} />
-						<Route path="/holding-page" element={<HoldingPage />} />
-						<Route path="/start-page" element={<StartPage />} />
-						<Route path="/race" element={<QuestionPage />} />
-					</Routes>
-				</Router>
+return (
+	<div className="page">
+		<div className="container">
+			{headingElement}
+			<div className="img-holder">
+				<img src={question} alt="" id="img" className="img" />
 			</div>
-	  	</div>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<LandingPage />} />
+					<Route exact path="/register" element={<RegisterPage />} />
+					<Route path="/holding-page" element={<HoldingPage />} />
+					<Route path="/start-page" element={<StartPage />} />
+					<Route path="/race" element={<QuestionPage />} />
+				</Routes>
+			</Router>
+		</div>
+	</div>
 	)
 }
 
